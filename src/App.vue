39f8f103e8cd92d8f12ee3f10721392034c4e6b6
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2017-09-09 14:47:17 -0300

    Adicionado NewQuote.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
 
     export default {
         data() {
@@ -16,9 +18,16 @@
                 maxQuotes: 10
             }
         },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote)
+            }
+        },
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
         }
+       
     }
 </script>
 
